4b35cf85f45863ee33cd05736cf5dcb7ddeddc84
Ozurmba chibuike <ocbmf17@gmail.com>
2024-02-05 01:24:43 +0100

    done
    done

---
+++ models/shipmentPackage.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const shipmentPackageSchema = new mongoose.Schema({
+    shipmentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Shipment'
+    },
+    productName: String,
+    productWeight: String,
+    productSize: String,
+    productDescription: String
+});
+
+const ShipmentPackage = mongoose.model('ShipmentPackage', shipmentPackageSchema);
+
+module.exports = ShipmentPackage;
